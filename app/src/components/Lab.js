442f96679624d8d2937f0e4cdd78b70aef6dbe5b
Chih-Yuan Su <csu68@gatech.edu>
2020-11-01 20:44:25 -0800

    Fix the lab refresh problem

--- app/src/components/Lab.js
+++ app/src/components/Lab.js
@@ -6,7 +6,7 @@ const Lab = () => {
         shiftX: 20,
         shiftY: 0
     }
-    const labs = ["Height", "Weight", "BMI", "HDL", "LDL", "Blood Pressure"]
+
     return (
         <div className="lab-container">
             <header className="header">
@@ -14,20 +14,66 @@ const Lab = () => {
             </header>
             <main className="main-lab">
                 <div className="lab-tests">
-                    {labs.map(lab => {
-                        return (
-                            <div className="lab-test">
-                                <ReactHover options={optionsCursorTrueWithMargin}>
-                                    <Trigger type="trigger">
-                                        <button className="lab-test-button">{lab}:</button>
-                                    </Trigger>
-                                    <Hover type="hover">
-                                        <p>Click to see results.</p>
-                                    </Hover>
-                                </ReactHover>
-                            </div>
-                        );
-                    })}
+                    <div className="lab-test">
+                        <ReactHover options={optionsCursorTrueWithMargin}>
+                            <Trigger type="trigger">
+                                <button className="lab-test-button">Height</button>
+                            </Trigger>
+                            <Hover type="hover">
+                                <p>Click to see Height.</p>
+                            </Hover>
+                        </ReactHover>
+                    </div>
+                    <div className="lab-test">
+                        <ReactHover options={optionsCursorTrueWithMargin}>
+                            <Trigger type="trigger">
+                                <button className="lab-test-button">Weight</button>
+                            </Trigger>
+                            <Hover type="hover">
+                                <p>Click to see Weight.</p>
+                            </Hover>
+                        </ReactHover>
+                    </div>
+                    <div className="lab-test">
+                        <ReactHover options={optionsCursorTrueWithMargin}>
+                            <Trigger type="trigger">
+                                <button className="lab-test-button">BMI</button>
+                            </Trigger>
+                            <Hover type="hover">
+                                <p>Click to see BMI.</p>
+                            </Hover>
+                        </ReactHover>
+                    </div>
+                    <div className="lab-test">
+                        <ReactHover options={optionsCursorTrueWithMargin}>
+                            <Trigger type="trigger">
+                                <button className="lab-test-button">HDL</button>
+                            </Trigger>
+                            <Hover type="hover">
+                                <p>Click to see HDL.</p>
+                            </Hover>
+                        </ReactHover>
+                    </div>
+                    <div className="lab-test">
+                        <ReactHover options={optionsCursorTrueWithMargin}>
+                            <Trigger type="trigger">
+                                <button className="lab-test-button">LDL</button>
+                            </Trigger>
+                            <Hover type="hover">
+                                <p>Click to see LDL.</p>
+                            </Hover>
+                        </ReactHover>
+                    </div>
+                    <div className="lab-test">
+                        <ReactHover options={optionsCursorTrueWithMargin}>
+                            <Trigger type="trigger">
+                                <button className="lab-test-button">Blood Pressure</button>
+                            </Trigger>
+                            <Hover type="hover">
+                                <p>Click to see Blood Pressure.</p>
+                            </Hover>
+                        </ReactHover>
+                    </div>
                 </div>
             </main>
         </div>
