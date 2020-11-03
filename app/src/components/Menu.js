38a3e95f1db4964259c4bb9cbd6a4d480f477439
Chih-Yuan Su <csu68@gatech.edu>
2020-11-03 00:08:10 -0800

    switch to display the real items

--- app/src/components/Menu.js
+++ app/src/components/Menu.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const Menu = () => {
-
-    return (<h1>Hello, world!</h1>);
-
     return (
         <div className="menu-container">
             <header className="header">
