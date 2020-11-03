f5964e5fa1f498c37e8687a0f27adeb197dffd9e
Chih-Yuan Su <csu68@gatech.edu>
2020-11-02 23:36:58 -0800

    commenting off the env

--- app/src/index.js
+++ app/src/index.js
@@ -3,8 +3,3 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('root')
-// );
