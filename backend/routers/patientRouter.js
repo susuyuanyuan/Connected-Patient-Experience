3347cf54b091bc839ee7b9fcefdc51426401277a
Chih-Yuan Su <csu68@gatech.edu>
2020-11-30 23:11:23 -0800

    Add message API in server

--- backend/routers/patientRouter.js
+++ backend/routers/patientRouter.js
@@ -28,4 +28,29 @@ patientRouter.get("/", (req, res) => {
   }
 });
 
+async function UpdateUserMessage(req, res) {
+  const input = req.body;
+  if (
+    !input ||
+    !input.message ||
+    !input.message.subject ||
+    !input.message.content ||
+    !input.userId
+  ) {
+    res.status(500).send("Invalid input data");
+  }
+  const patient = await Patient.findById(input.userId);
+  patient.messages.push(input.message);
+  const result = await patient.updateOne({ messages: patient.messages });
+  res.sendStatus(200);
+}
+
+patientRouter.post("/message", (req, res) => {
+  try {
+    UpdateUserMessage(req, res);
+  } catch (err) {
+    res.status(500).send("Update message failed due to: " + err);
+  }
+});
+
 module.exports = patientRouter;
